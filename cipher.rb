6a8625a28cb7fab897c77ac23076575d8b3f4566
harkato <92404938+harkato@users.noreply.github.com>
2023-08-25 23:36:49 -0300

    Convert characters to numbers

--- cipher.rb
+++ cipher.rb
@@ -1,5 +1,13 @@
 def caesar_cipher(string, shift_factor)
-    puts string, shift_factor
+    separated_string = string.split(//)
+    characters_numbers = character_to_number(separated_string)
+    p characters_numbers
 end
 
-caesar_cipher("hello!", 5)
+def character_to_number(array)
+    array.each_with_index do |character, index|
+        array[index] = character.ord
+    end
+end
+
+caesar_cipher("Hello World!", 5)
