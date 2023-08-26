ada8cb1549ddae0f01c855b3ab2e192e783337fb
harkato <92404938+harkato@users.noreply.github.com>
2023-08-26 00:58:43 -0300

    Finish caesar cipher algorithm

--- cipher.rb
+++ cipher.rb
@@ -1,8 +1,10 @@
 def caesar_cipher(string, shift_factor)
+    modified_string = number_to_character(number_shift(character_to_number(separate_string(string)), shift_factor)).join
+    p modified_string
+end
+
+def separate_string(string)
     separated_string = string.split(//)
-    characters_numbers = character_to_number(separated_string)
-    shifted_numbers = number_shift(characters_numbers, shift_factor)
-    p characters_numbers, shifted_numbers
 end
 
 def character_to_number(array)
@@ -12,22 +14,33 @@ def character_to_number(array)
 end
 
 def number_shift(array, shift_factor)
-    array.each do |number|
+    shifted_array = []
+    modified_num = 0
+    array.each_with_index do |number, index|
         if number.between?(65,90) or number.between?(97,122)
-            number += shift_factor
-            case number
-            when number>90
-                shift_factor = number-90
-                number = 65 + shift_factor
-                return number
-            when number>122
-                shift_factor = number-122
-                number = 97 + shift_factor
-                return number
+            modified_num = number + shift_factor
+            if modified_num>122 and number.between?(97,122)
+                shift_factor_updated = modified_num-122
+                modified_num = 96 + shift_factor_updated
+            elsif modified_num>90 and number.between?(65,90)
+                shift_factor_updated = modified_num-90
+                modified_num = 64 + shift_factor_updated
             end
-            number
+            shifted_array[index] = modified_num
+        else
+            shifted_array[index] = number
         end
     end
+    shifted_array
 end
 
-caesar_cipher("Hello World!", 5)
+def number_to_character(array)
+    array.each_with_index do |character, index|
+        array[index] = character.chr
+    end
+end
+puts "Text something: "
+string = gets.chomp
+puts "Encription key (number): "
+shift_factor = gets.chomp.to_i
+caesar_cipher(string, shift_factor)
