92099f679814db24c7a52e06771c58d36b737d1c
harkato <92404938+harkato@users.noreply.github.com>
2023-08-25 23:56:26 -0300

    Define function number_shift, needs fix

--- cipher.rb
+++ cipher.rb
@@ -1,12 +1,32 @@
 def caesar_cipher(string, shift_factor)
     separated_string = string.split(//)
     characters_numbers = character_to_number(separated_string)
-    p characters_numbers
+    shifted_numbers = number_shift(characters_numbers, shift_factor)
+    p characters_numbers, shifted_numbers
 end
 
 def character_to_number(array)
     array.each_with_index do |character, index|
         array[index] = character.ord
+    end
+end
+
+def number_shift(array, shift_factor)
+    array.each do |number|
+        if number.between?(65,90) or number.between?(97,122)
+            number += shift_factor
+            case number
+            when number>90
+                shift_factor = number-90
+                number = 65 + shift_factor
+                return number
+            when number>122
+                shift_factor = number-122
+                number = 97 + shift_factor
+                return number
+            end
+            number
+        end
     end
 end
 
